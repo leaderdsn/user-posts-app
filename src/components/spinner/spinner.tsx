c92913335506777c72ee518eee8e34b476ce4d23
leaderdsn <leaderdsn.it@gmail.com>
2021-03-07 17:41:08 +0800

    created a file spinner

---
+++ src/components/spinner/spinner.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./spinner.css";
+
+const Spinner: React.FC = () => {
+  return (
+    <div className="loadingio-spinner-pulse-eb5jci4p1p">
+      <div className="ldio-yw76ita4v87">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
