2ebe01ff897d945477e872853a5f32146208b304
leaderdsn <leaderdsn.it@gmail.com>
2021-03-11 06:41:21 +0800

    added pagination

--- src/pages/post-details/post-details.tsx
+++ src/pages/post-details/post-details.tsx
@@ -1,6 +1,31 @@
 import React from 'react';
+import { useRootStore } from '../../stores/RootStateContext';
+import { Pagination } from '../../components/pagination';
 import { ListComments } from '../../components/list-comments';
+import { observer } from 'mobx-react-lite';
 
-export const PostDetails = () => {
-    return <ListComments/>
-}
+export const PostDetails: React.FC = observer(() => {
+    const {commentsStore} = useRootStore();
+    const {comments, isLoading, pagination} = commentsStore;
+    const paginate = (pageNumber: number) => commentsStore.loadComments(pageNumber)
+    return (
+        <>
+            <ListComments
+                comments={comments}
+                page={pagination?.page}
+                isLoading={isLoading}
+            />
+            {
+                !isLoading ? (
+                    <Pagination
+                        pages={pagination?.pages}
+                        page={pagination?.page}
+                        total={pagination?.total}
+                        limit={pagination?.limit}
+                        paginate={paginate}
+                    />
+                ) : null
+            }
+        </>
+    )
+})
