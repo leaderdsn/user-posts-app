774d1121800268dded49a194ccd70e570ae4551b
leaderdsn <leaderdsn.it@gmail.com>
2021-03-10 19:38:34 +0800

    added posts and isLoading typing

--- src/components/list-posts/list-posts.tsx
+++ src/components/list-posts/list-posts.tsx
@@ -1,19 +1,22 @@
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router';
-import { observer } from 'mobx-react-lite';
 import { Link } from 'react-router-dom';
 import { useRootStore } from '../../stores/RootStateContext';
-import { Button, Table } from 'react-bootstrap';
-import Spinner from '../../components/spinner'
+import { Table } from 'react-bootstrap';
+import Spinner from '../../components/spinner';
 import './list-posts.css';
+import { IPost } from '../../stores/PostStore';
 interface RouteParams {
     id: string
 }
+interface ListPostsProps {
+    posts: IPost[]
+    isLoading?: boolean
+}
 
-export const ListPosts: React.FC = observer(() => {
+export const ListPosts: React.FC<ListPostsProps> = ({posts, isLoading}) => {
     const {id} = useParams<RouteParams>();
     const{postsStore} = useRootStore();
-    const isLoading = postsStore.isLoading;
 
     useEffect(() => {
         postsStore.loadPosts();
@@ -28,28 +31,30 @@ export const ListPosts: React.FC = observer(() => {
             </div>
             <Table className='list-posts' bordered hover size="sm">
                     <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Post Title</th>
-                        <th>Date Created</th>
-                        <th>Date Updated</th>
-                        <th>Action</th>
-                    </tr>
+                        <tr>
+                            <th>#</th>
+                            <th>Post Title</th>
+                            <th>Post Body</th>
+                            <th>Date Created</th>
+                            <th>Date Updated</th>
+                            <th>Action</th>
+                        </tr>
                     </thead>
                     <tbody>
                     {
                         isLoading ? (
-                            <td className='loading-container' colSpan={5}>
+                            <td className='loading-container' colSpan={6}>
                                 <Spinner />
                             </td>
                         ) : (
-                            postsStore.posts.map(post => {
-                                const {user_id, title, created_at, updated_at} = post
+                            posts.map(post => {
+                                const {user_id, title, body, created_at, updated_at} = post
                                 if(Number(id) === user_id){
                                     return (
-                                        <tr key={user_id}>
+                                        <tr key={user_id} className='item'>
                                             <td>{user_id}</td>
                                             <td>{title}</td>
+                                            <td>{body}</td>
                                             <td>{created_at}</td>
                                             <td>{updated_at}</td>
                                             <td>
@@ -63,11 +68,6 @@ export const ListPosts: React.FC = observer(() => {
                     }
                     </tbody>
                 </Table>
-                <div className='pangination-container'>
-                    <Button className='mx-2' variant={isLoading ? 'secondary': 'success'} disabled={isLoading}>
-                        {isLoading ? 'Loading…' : 'Load More'}
-                    </Button>
-                </div>
         </>
     )
-})
+}
