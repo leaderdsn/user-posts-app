533e5119946c72ce1d38ce331a8990a55014ca6a
leaderdsn <leaderdsn.it@gmail.com>
2021-03-11 06:40:07 +0800

    added choice item

--- src/components/list-users/list-users.tsx
+++ src/components/list-users/list-users.tsx
@@ -1,55 +1,76 @@
-import React from 'react';
-import { Table } from 'react-bootstrap';
+import React, { MouseEvent, useEffect, useState } from 'react';
+import { Table, Form } from 'react-bootstrap';
 import Spinner from '../spinner';
 import './list-users.css';
 import { Link } from 'react-router-dom';
 import { IUser} from '../../stores/UsersStore';
+import { observer } from 'mobx-react-lite';
 
 interface ListUsersProps {
     users: IUser[]
     isLoading?: boolean
 }
 
-export const ListUsers: React.FC<ListUsersProps> = ({users, isLoading}) => {
+export const ListUsers: React.FC<ListUsersProps> = observer(({users, isLoading}) => {
+    const[itemChecked, setItemChecked] = useState<Boolean>(false)
+
+    const onCheckedHandler = (e:React.ChangeEvent<HTMLInputElement>, id:number) => {
+        if(e.target.checked){  
+            console.log(e)
+            return setItemChecked(true)
+        }  
+        setItemChecked(false)
+    }
+
 
     return (
         <>
-            <Table className='list-users' bordered hover size='sm'>
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>User name</th>
-                        <th>Gender</th>
-                        <th>Status</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                {
-                    isLoading ? (
-                        <td className='loading-container' colSpan={5}>
-                            <Spinner />
-                        </td>
-                    ) : (
-                        users.map(user => {
-                            const {id, name, gender, status} = user
-
-                            return (
-                                <tr key={id} className="item">
-                                    <td>{id}</td>
-                                    <td>{name}</td>
-                                    <td>{gender}</td>
-                                    <td>{status}</td>
-                                    <td>
-                                        <Link className='btn btn-primary btn-sm' to={`/posts/${id}/`}>Go to posts</Link>
-                                    </td>
-                                </tr>
-                            )
-                        })
-                    )
-                }
-                </tbody>
-            </Table>
+            <div className='wrapper'>
+                <Table className='list-users' bordered hover size='sm'>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Choice</th>
+                            <th>User name</th>
+                            <th>Gender</th>
+                            <th>Status</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        isLoading ? (
+                            <tr>
+                                <td className='loading-container' colSpan={6}>
+                                    <Spinner />
+                                </td>
+                            </tr>
+                            
+                        ) : (
+                            users.map(user => {
+                                const {id, name, gender, status} = user
+                                return (
+                                    <tr key={id} className="item">
+                                        <td className='id-col'>{id}</td>
+                                        <td className='choice-col'>
+                                            <Form.Check type="checkbox" 
+                                                onChange={(e)=> onCheckedHandler(e, id)} 
+                                            />
+                                        </td>
+                                        <td>{name}</td>
+                                        <td>{gender}</td>
+                                        <td>{status}</td>
+                                        <td className="action-col">
+                                            <Link id={`btn-${id}`} className={`btn btn-sm ${itemChecked ? 'btn-secondary disabled-link': 'btn-primary' }`} to={`/posts/${id}/`} >Go to posts</Link>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        )
+                    }
+                    </tbody>
+                </Table>
+            </div>
         </>
     )
-}
+})
