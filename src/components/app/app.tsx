03a2d819e45a588fa21797d93cef71c50e218202
leaderdsn <leaderdsn.it@gmail.com>
2021-03-11 06:37:45 +0800

    formating code

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,33 +1,36 @@
-import React from 'react';
-import {configure} from 'mobx'
-import {BrowserRouter as Router, Redirect, Route, Switch} from 'react-router-dom';
-import {Home} from '../../pages/home';
-import {Posts} from '../../pages/posts';
-import {PostDetails} from '../../pages/post-details';
-import './app.css';
+import { configure } from "mobx";
+import {
+    BrowserRouter as Router,
+    Redirect,
+    Route,
+    Switch,
+} from "react-router-dom";
+import { Home } from "../../pages/home";
+import { Posts } from "../../pages/posts";
+import { PostDetails } from "../../pages/post-details";
+import "./app.css";
 
 configure({
-    enforceActions: 'always',
+    enforceActions: "always",
     computedRequiresReaction: true,
     reactionRequiresObservable: true,
     observableRequiresReaction: true,
-    disableErrorBoundaries: true
-})
+    disableErrorBoundaries: true,
+});
 
 export const App = () => {
-  return (
-      <>
-          <div className="app">
-          <Router>
-              <Switch>
-                  <Route path='/' exact component={Home} />
-                  <Route path='/posts/:id/' exact component={Posts} />
-                  <Route path='/post-details/:id/' exact component={PostDetails} />
-                  <Redirect to='/' />
-                  {/* <DevTools/> */}
-              </Switch>
-          </Router>
-          </div>  
-      </>
-  )
-}
+    return (
+        <>
+        <div className="app">
+            <Router>
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/posts/:id/" exact component={Posts} />
+                <Route path="/post-details/:id/" exact component={PostDetails} />
+                <Redirect to="/" />
+            </Switch>
+            </Router>
+        </div>
+        </>
+    );
+};
