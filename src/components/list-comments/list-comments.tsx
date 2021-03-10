135bb87ed7aa6a1aaf2bb8704f9edd4daf0b8e0f
leaderdsn <leaderdsn.it@gmail.com>
2021-03-11 06:37:57 +0800

    added interface ListCommentsProps

--- src/components/list-comments/list-comments.tsx
+++ src/components/list-comments/list-comments.tsx
@@ -1,77 +1,78 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
-import {observer} from 'mobx-react-lite';
 import {Link} from 'react-router-dom';
 import { useRootStore } from '../../stores/RootStateContext';
-import {Button, Table} from 'react-bootstrap';
-import Spinner from '../spinner'
+import {Table, Button, Form} from 'react-bootstrap';
+import Spinner from '../spinner';
+import { IComment } from '../../stores/CommentsStore';
 import './list-comments.css';
 
 interface RouteParams {
     id: string
 }
 
-export const ListComments: React.FC = observer(() => {
-    const {id} = useParams<RouteParams>();
+interface ListCommentsProps {
+    comments: IComment[]
+    isLoading?: boolean
+    page?: number
+}
+
+export const ListComments: React.FC<ListCommentsProps> = ({comments, isLoading, page}) => {
+    const {id: idComments} = useParams<RouteParams>();
     const{commentsStore} = useRootStore();
-    const isLoading = commentsStore.isLoading;
 
     useEffect(()=>{
-        commentsStore.loadComments();
-    },[commentsStore])
+        commentsStore.loadComments(page, Number(idComments));
+    },[idComments, page, commentsStore])
 
     return(
         <>
-            <h3 className="user-id">Post id:{id}</h3>
-            <div className='btn-container'>
-                <Link className='btn btn-outline-primary my-2' to={`/posts/${id}/`}>Back</Link>
-            </div>
-            <Table className='list-posts' bordered hover size="sm">
+            <div className='wrapper'>
+                <h3 className="user-id">Post id:{idComments}</h3>
+                <div className='btn-container'>
+                    <Link className='btn btn-outline-primary my-2 btn-sm' to={`/posts/${idComments}`}>Back</Link>
+                </div>
+                <Table className='list-posts' bordered hover size="sm">
                     <thead>
                     <tr>
                         <th>#</th>
+                        <th>Choice</th>
                         <th>User name</th>
                         <th>Comment</th>
-                        <th>Date Created</th>
-                        <th>Date Updated</th>
                         <th>Action</th>
                     </tr>
                     </thead>
                     <tbody>
                     {
                         isLoading ? (
-                            <td className='loading-container' colSpan={6}>
-                                <Spinner />
-                            </td>
+                            <tr>
+                                <td className='loading-container' colSpan={5}>
+                                    <Spinner />
+                                </td>
+                            </tr>
                         ) : (
-                            commentsStore.comments.map(comment => {
-                                const {post_id, name, body, created_at, updated_at} = comment
-                                if(Number(id) === post_id){
+                            comments.map(comment => {
+                                const {id, post_id, name, body} = comment
                                     return (
-                                        <tr key={post_id}>
-                                            <td>{post_id}</td>
+                                        <tr key={id}>
+                                            <td>{id}</td>
+                                            <td className='choice-col'>
+                                                <Form.Check type="checkbox" 
+                                                />
+                                            </td>
                                             <td>{name}</td>
                                             <td>{body}</td>
-                                            <td>{created_at}</td>
-                                            <td>{updated_at}</td>
-                                            <td>
-                                                <Link className='btn btn-primary btn-sm' to={`/post-details/${id}/`}>Show</Link>
+                                            <td className='action-col'>
+                                                <Button className='btn btn-primary btn-sm'>Show</Button>
                                             </td>
                                         </tr>
                                     )
-                                }
                             })
                         )
                     }
                     </tbody>
                 </Table>
-                <div className='pangination-container'>
-                    <Button className='mx-2 ' variant={isLoading ? 'secondary': 'success'} disabled={isLoading}>
-                        {isLoading ? 'Loading…' : 'Load More'}
-                    </Button>
-                    
-                </div>
-            
+            </div>
         </>
     )
-})
+}
