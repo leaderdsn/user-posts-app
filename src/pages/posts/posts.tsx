1d926f2aeb2f28b022445dee2b395691a547fdbf
leaderdsn <leaderdsn.it@gmail.com>
2021-03-10 19:43:05 +0800

    Added pagination

--- src/pages/posts/posts.tsx
+++ src/pages/posts/posts.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
-import { ListPosts } from '../../components/list-posts'
+import { ListPosts } from '../../components/list-posts';
+import {useRootStore} from '../../stores/RootStateContext';
+import {PostsPagination} from '../../components/posts-pagination';
+import { observer } from 'mobx-react-lite';
 
-export const Posts = () => {
+export const Posts: React.FC = observer(() => {
+    const {postsStore} = useRootStore();
+    const {posts, isLoading, pagination} = postsStore;
+    const paginate = (pageNumber: number) => postsStore.loadPosts(pageNumber)
     return (
-        <ListPosts/>
+        <>
+            <ListPosts
+                posts={posts}
+                isLoading={isLoading}
+            />
+            {
+                !isLoading ? (
+                    <PostsPagination
+                        pages={pagination?.pages}
+                        page={pagination?.page}
+                        total={pagination?.total}
+                        limit={pagination?.limit}
+                        paginate={paginate}
+                    />
+                ) : null
+            }
+        </>
     )
-}
+})
