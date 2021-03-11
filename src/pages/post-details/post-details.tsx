8c3fdf8a9b2de3c2187d33efd28072b3de5c16c2
leaderdsn <leaderdsn.it@gmail.com>
2021-03-11 15:12:03 +0800

    configured the correct pagination display when loading

--- src/pages/post-details/post-details.tsx
+++ src/pages/post-details/post-details.tsx
@@ -1,22 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useRootStore } from '../../stores/RootStateContext';
 import { Pagination } from '../../components/pagination';
 import { ListComments } from '../../components/list-comments';
 import { observer } from 'mobx-react-lite';
+import { useParams } from 'react-router';
+
+interface RouteParams {
+    id: string
+}
 
 export const PostDetails: React.FC = observer(() => {
+    const { id: postId } = useParams<RouteParams>();
     const {commentsStore} = useRootStore();
     const {comments, isLoading, pagination} = commentsStore;
+    const hasComments = !!comments.length
+    const showPagination = !isLoading && hasComments
     const paginate = (pageNumber: number) => commentsStore.loadComments(pageNumber)
+    
+    useEffect(() => {
+        commentsStore.loadComments(pagination?.page, Number(postId));
+
+        return () => {
+            commentsStore.reset()
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+    
     return (
         <>
             <ListComments
+                postId={postId}
                 comments={comments}
-                page={pagination?.page}
                 isLoading={isLoading}
             />
             {
-                !isLoading ? (
+                showPagination && (
                     <Pagination
                         pages={pagination?.pages}
                         page={pagination?.page}
@@ -24,7 +42,7 @@ export const PostDetails: React.FC = observer(() => {
                         limit={pagination?.limit}
                         paginate={paginate}
                     />
-                ) : null
+                )
             }
         </>
     )
