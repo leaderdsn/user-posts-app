0fd413954e1cc18464d5817e2f11df4c8f074353
leaderdsn <leaderdsn.it@gmail.com>
2021-03-11 15:11:27 +0800

    configured the correct pagination display when loading

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import {Menu} from '../../components/menu';
-import {ListUsers} from '../../components/list-users';
-import {useRootStore} from '../../stores/RootStateContext';
-import {Pagination} from '../../components/pagination';
+import { Menu } from '../../components/menu';
+import { ListUsers } from '../../components/list-users';
+import { useRootStore } from '../../stores/RootStateContext';
+import { Pagination } from '../../components/pagination';
 import { observer } from 'mobx-react-lite';
 
-
 export const Home: React.FC = observer(() => {
-    const {usersStore} = useRootStore();
-    const {users, isLoading, pagination} = usersStore;
+    const { usersStore } = useRootStore();
+    const { users, isLoading, pagination } = usersStore;
+    const hasUsers = !!users.length
+    const showPagination = !isLoading && hasUsers
     const paginate = (pageNumber: number) => usersStore.loadUsers(pageNumber);
-    
+
     return (
         <>
             <Menu />
@@ -19,17 +20,15 @@ export const Home: React.FC = observer(() => {
                 isLoading={isLoading}
             />
             {
-                isLoading ? (
-                    null
-                ) : (
+                showPagination && (
                     <Pagination
                         pages={pagination?.pages}
                         page={pagination?.page}
                         total={pagination?.total}
                         limit={pagination?.limit}
                         paginate={paginate}
-                    /> 
-                ) 
+                    />
+                )
             }
         </>
     );
--- src/pages/posts/posts.tsx
+++ src/pages/posts/posts.tsx
@@ -1,22 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ListPosts } from '../../components/list-posts';
 import { useRootStore } from '../../stores/RootStateContext';
 import { Pagination } from '../../components/pagination';
 import { observer } from 'mobx-react-lite';
+import { useParams } from 'react-router';
+
+interface RouteParams {
+    id: string
+}
 
 export const Posts: React.FC = observer(() => {
-    const {postsStore} = useRootStore();
-    const {posts, isLoading, pagination} = postsStore;
+    const { id: userId } = useParams<RouteParams>();
+    const { postsStore } = useRootStore();
+    const { posts, isLoading, pagination } = postsStore;
+    const hasPosts = !!posts.length
+    const showPagination = !isLoading && hasPosts
     const paginate = (pageNumber: number) => postsStore.loadPosts(pageNumber)
+
+    useEffect(() => {
+        postsStore.loadPosts(pagination?.page, Number(userId));
+
+        return () => {
+            postsStore.reset()
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
     return (
         <>
             <ListPosts
+                userId={userId}
                 posts={posts}
-                page={pagination?.page}
                 isLoading={isLoading}
             />
             {
-                !isLoading ? (
+                showPagination && (
                     <Pagination
                         pages={pagination?.pages}
                         page={pagination?.page}
@@ -24,7 +42,7 @@ export const Posts: React.FC = observer(() => {
                         limit={pagination?.limit}
                         paginate={paginate}
                     />
-                ) : null
+                )
             }
         </>
     )
