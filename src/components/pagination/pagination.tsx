ac9779b20f9bca172dda7a49c9c26fc73a3ce5be
leaderdsn <leaderdsn.it@gmail.com>
2021-03-11 15:09:00 +0800

    simplified the pagination logic

--- src/components/pagination/pagination.tsx
+++ src/components/pagination/pagination.tsx
@@ -1,85 +1,41 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 import {PageItem, Pagination as Paginate, } from 'react-bootstrap';
 import './pagination.css'
-import {IPagination} from '../../stores/UsersStore';
+import {IPagination} from '../../interfaces/pagination';
 
 interface PaginationProps extends IPagination {
     paginate: (page: number) => void
 }
 
 export const Pagination: React.FC<PaginationProps> = ({ pages = 0, page = 1, total = 0, limit = 20, paginate}) => {
-    const content: ReactNode[] = [];
-    const pagesCount = Math.ceil(total/limit)
-
-    if(pages > limit){
-        if(page > Math.round(limit/4)){
-            for (let i = page-(Math.round(limit/8)-1); i <= page+(Math.round(limit/8)); i++) {
-                content.push((
-                    <PageItem
-                        key={i}
-                        active={i === page}
-                        onClick={() => paginate(i)}
-                    >
-                        {i}
-                    </PageItem>
-                ))
-                if(i === pagesCount) break
-            }
-        } else {
-            for (let i = 1; i <=  Math.round(limit/4); i++) {
-                content.push((
-                    <PageItem
-                        key={i}
-                        active={i === page}
-                        onClick={() => paginate(i)}
-                    >
-                        {i}
-                    </PageItem>
-                ))
-                if(i === pagesCount) break
-            }
-        }
-    } else {
-        for (let i = 1; i <= pagesCount; i++) {
-            content.push((
-                <PageItem
-                    key={i}
-                    active={i === page}
-                    onClick={() => paginate(i)}
-                >
-                    {i}
-                </PageItem>
-            ))
-        }
-    }
 
     return (
         <Paginate className='pagination' size='sm'>
             <PageItem
                 onClick={()=> paginate(1)}
-                disabled={ page !== 1 ? false : true }   
+                disabled={ page === 1 }
             >
                 ❮❮
             </PageItem>
             <PageItem
                 onClick={()=> page === 1 ? null : paginate(page - 1)}
-                disabled={ page !== 1 ? false : true }   
+                disabled={ page === 1 }
             >
                 ❮
             </PageItem>
-            {
-                content
-            }
+            <PageItem disabled>
+                {`Page ${page} of ${pages}`}
+            </PageItem>
             <PageItem
                 onClick={()=> page === pages ? null : paginate(page + 1)}
-                disabled={ page === pages || pages === 0 ? true : false }
+                disabled={ page === pages || pages === 0 }
             >
                 ❯
             </PageItem>
             <PageItem
                 className='page-last'
                 onClick={()=> paginate(pages)}
-                disabled={ page === pages || pages === 0 ? true : false }   
+                disabled={ page === pages || pages === 0 }
             >
                 ❯❯
             </PageItem>
