b26ccac77f294b5170f52ec7503159ba634662dc
leaderdsn <leaderdsn.it@gmail.com>
2021-03-11 15:06:25 +0800

    I moved the api call to pages

--- src/components/list-comments/list-comments.tsx
+++ src/components/list-comments/list-comments.tsx
@@ -1,75 +1,54 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
-import {Link} from 'react-router-dom';
-import { useRootStore } from '../../stores/RootStateContext';
-import {Table, Button, Form} from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Table, Form } from 'react-bootstrap';
 import Spinner from '../spinner';
 import { IComment } from '../../stores/CommentsStore';
 import './list-comments.css';
 
-interface RouteParams {
-    id: string
-}
-
 interface ListCommentsProps {
     comments: IComment[]
     isLoading?: boolean
-    page?: number
+    postId: string
 }
 
-export const ListComments: React.FC<ListCommentsProps> = ({comments, isLoading, page}) => {
-    const {id: idComments} = useParams<RouteParams>();
-    const{commentsStore} = useRootStore();
+export const ListComments: React.FC<ListCommentsProps> = ({ postId, comments, isLoading }) => {
 
-    useEffect(()=>{
-        commentsStore.loadComments(page, Number(idComments));
-    },[idComments, page, commentsStore])
-
-    return(
+    return (
         <>
             <div className='wrapper'>
-                <h3 className="user-id">Post id:{idComments}</h3>
+                <h3 className="user-id">Post id:{postId}</h3>
                 <div className='btn-container'>
-                    <Link className='btn btn-outline-primary my-2 btn-sm' to={`/posts/${idComments}`}>Back</Link>
+                    <Link className='btn btn-outline-primary my-2 btn-sm' to={`/posts/${postId}`}>Back</Link>
                 </div>
                 <Table className='list-posts' bordered hover size="sm">
                     <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Choice</th>
-                        <th>User name</th>
-                        <th>Comment</th>
-                        <th>Action</th>
-                    </tr>
+                        <tr>
+                            <th>#</th>
+                            <th>User name</th>
+                            <th>Comment</th>
+                        </tr>
                     </thead>
                     <tbody>
-                    {
-                        isLoading ? (
-                            <tr>
-                                <td className='loading-container' colSpan={5}>
-                                    <Spinner />
-                                </td>
-                            </tr>
-                        ) : (
-                            comments.map(comment => {
-                                const {id, post_id, name, body} = comment
+                        {
+                            isLoading ? (
+                                <tr>
+                                    <td className='loading-container' colSpan={3}>
+                                        <Spinner />
+                                    </td>
+                                </tr>
+                            ) : (
+                                comments.map(comment => {
+                                    const { id, post_id, name, body } = comment
                                     return (
                                         <tr key={id}>
                                             <td>{id}</td>
-                                            <td className='choice-col'>
-                                                <Form.Check type="checkbox" 
-                                                />
-                                            </td>
                                             <td>{name}</td>
                                             <td>{body}</td>
-                                            <td className='action-col'>
-                                                <Button className='btn btn-primary btn-sm'>Show</Button>
-                                            </td>
                                         </tr>
                                     )
-                            })
-                        )
-                    }
+                                })
+                            )
+                        }
                     </tbody>
                 </Table>
             </div>
--- src/components/list-posts/list-posts.tsx
+++ src/components/list-posts/list-posts.tsx
@@ -1,80 +1,79 @@
-import React, { useEffect } from 'react';
-import { useParams } from 'react-router';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useRootStore } from '../../stores/RootStateContext';
-import { Table } from 'react-bootstrap';
+import { Table, Form } from 'react-bootstrap';
 import Spinner from '../../components/spinner';
 import { IPost } from '../../stores/PostsStore';
 import './list-posts.css';
-interface RouteParams {
-    id: string
-}
+
 interface ListPostsProps {
     posts: IPost[]
     isLoading?: boolean
-    page?: number
+    userId:string
 }
 
-export const ListPosts: React.FC<ListPostsProps> = ({posts, isLoading, page}) => {
-    const {id: userId} = useParams<RouteParams>();
-    const{postsStore} = useRootStore();
+export const ListPosts: React.FC<ListPostsProps> = ({ userId, posts, isLoading }) => {
+    const [itemsChecked, setItemsChecked] = useState<number[]>([])
 
-    useEffect(() => {
-        let mounted = true;
-
-        if(mounted){
-            postsStore.loadPosts(page, Number(userId));
+    const onCheckedHandler = (e: React.ChangeEvent<HTMLInputElement>, id: number) => {
+        if (e.target.checked) {
+            setItemsChecked([...itemsChecked, id])
+        } else {
+            setItemsChecked([...itemsChecked.filter(item => item !== id)])
         }
-        
-        return() => {
-            mounted = false;
-        }
-    },[userId, page, postsStore])
+    }
 
-    return(
-        <>  
-        <div className='wrapper'>
-            <h3 className="user-id">User id:{userId}</h3>
-            <div className='btn-container'>
-                <Link className='btn btn-outline-primary my-2 btn-sm' to={`/`}>Back</Link>
-            </div>
-            <Table className='list-posts' bordered hover size="sm">
+    return (
+        <>
+            <div className='wrapper'>
+                <h3 className="user-id">User id:{userId}</h3>
+                <div className='btn-container'>
+                    <Link className='btn btn-outline-primary my-2 btn-sm' to={`/`}>Back</Link>
+                </div>
+                <Table className='list-posts' bordered hover size="sm">
                     <thead>
                         <tr>
                             <th>#</th>
+                            <th>Choice</th>
                             <th>Post Title</th>
                             <th>Post Body</th>
                             <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
-                    {
-                        isLoading ? (
-                            <tr>
-                                <td className='loading-container' colSpan={4}>
-                                    <Spinner />
-                                </td>
-                            </tr>
-                            
-                        ) : (
-                            posts.map(post => {
-                                const {id, user_id, title, body} = post
-                                return (
-                                    <tr key={user_id} className='item'>
-                                        <td className='id-col'>{id}</td>
-                                        <td>{title}</td>
-                                        <td>{body}</td>
-                                        <td className='action-col'>
-                                            <Link className='btn btn-primary btn-sm' to={`/post-details/${user_id}/`}>Show</Link>
-                                        </td>
-                                    </tr>
-                                )
-                            })
-                        )
-                    }
+                        {
+                            isLoading ? (
+                                <tr>
+                                    <td className='loading-container' colSpan={5}>
+                                        <Spinner />
+                                    </td>
+                                </tr>
+
+                            ) : (
+                                posts.map(post => {
+                                    const { id, user_id, title, body } = post
+                                    const isItemChecked = itemsChecked.includes(id)
+                                    return (
+                                        <tr key={user_id} className='item'>
+                                            <td className='id-col'>{id}</td>
+                                            <td className='choice-col'>
+                                                <Form.Check type="checkbox"
+                                                    onChange={(e) => onCheckedHandler(e, id)}
+                                                />
+                                            </td>
+                                            <td>{title}</td>
+                                            <td>{body}</td>
+                                            <td className='action-col'>
+                                            <Link className={`btn btn-sm ${isItemChecked ? 'btn-primary' : 'btn-secondary disabled-link'}`} 
+                                                    to={`/post-details/${id}/`}>Show</Link>
+                                            </td>
+                                        </tr>
+                                    )
+                                })
+                            )
+                        }
                     </tbody>
                 </Table>
-            </div> 
+            </div>
         </>
     )
 }
