b5cb801010becd621077c35e97bfd54c8c006dd7
leaderdsn <leaderdsn.it@gmail.com>
2021-03-14 03:52:08 +0800

    changed the display of the table with comments

--- src/components/list-comments/list-comments.tsx
+++ src/components/list-comments/list-comments.tsx
@@ -5,82 +5,49 @@ import { IComment } from '../../stores/CommentsStore';
 import './list-comments.css';
 
 interface ListCommentsProps {
-    comments: IComment[]
-    isLoading?: boolean
-    postId: string
+    comments: IComment[];
+    isLoading?: boolean;
 }
 
-// @ts-ignore
-// const commentsShow: React.FC<{comments: IComment[]}> = ({comments}) => {
-//     comments.map(comment => {
-//         const { id, name, body } = comment
-//         return (
-//             <>
-//                 <tr key={id}>
-//                     <tr>
-//                         <td>{id}</td>
-//                     </tr>
-//                     <tr>
-//                         <td>{name}</td>
-//                     </tr>
-//                     <tr>
-//                         <td>{body}</td>
-//                     </tr>
-//                     {
-//                         <tr>
-//                             {
-//                                 // @ts-ignore
-//                                 comment.children ? commentsShow(comment.children): null
-//                             } 
-//                         </tr>
-//                     }
-//                 </tr>
-//             </>
-//         )
-//     })
-//     return
-// }
+export const ListComments: React.FC<ListCommentsProps> = ({ comments, isLoading }) => {
+    if (isLoading) {
+        return (
+            <div className='loading-comments text-center'>
+                <Spinner />
+            </div>
+        )
+    } 
 
-export const ListComments: React.FC<ListCommentsProps> = ({ postId, comments, isLoading }) => {
+    const hasComments = !!comments.length;
 
     return (
-        <>
-            <h3 className="user-id">Comments</h3>
-                    {
-                        isLoading ? (
-                            <div className='loading-container'>
-                                <Spinner />
-                            </div>
-                        ) : (
-                            // @ts-ignore
-                            // commentsShow(comments)
-                            comments.map(comment => {
-                                const { id, name, body } = comment
-                                return (
-                                    <>
-                                    <Table className='list-post' hover size="sm" responsive>
-                                        <thead>
-
-                                        </thead>
-                                        <tbody>
-                                            <tr key={`comment-${id}`} className='comments-container'>
-                                            </tr>   
-                                            <tr>
-                                                <th className='id-comment align-middle text-center' rowSpan={2}>ID: {id}</th>
-                                                <th className='header-comment'>Name user</th>
-                                                <td>{name}</td>
-                                            </tr>
-                                            <tr>
-                                                <th className='header-comment'>Comment</th>
-                                                <td>{body}</td>
-                                            </tr>
-                                        </tbody>
-                                    </Table>
-                                    </>
-                                )
-                            })
-                        )
-                    }
-        </>
-    )
-}
+        hasComments ? (
+            <>  
+                <div className='comments-container'>
+                    <h4 className='header-comments'>Comments</h4>
+                    {comments.map(comment => {
+                        const { id, name, body } = comment
+                        return (
+                            <>
+                                <Table key={id} className='comment-container' size='sm' hover responsive>
+                                    <tbody>
+                                        <tr>
+                                            <th className='header-comment-id align-middle'>ID: {id}</th>
+                                            <th className='header-comment-name'>Name user</th>
+                                            <td>{name}</td>
+                                        </tr>
+                                        <tr>
+                                            <th></th>
+                                            <th className='header-comment-body'>Comment</th>
+                                            <td>{body}</td>
+                                        </tr>
+                                    </tbody>
+                                </Table>
+                            </>
+                        );
+                    })}
+                </div>
+            </>
+        ) : null
+    );
+};
