e2041a094c1608c98d3b5f8c7e8c1892e3f78d22
leaderdsn <leaderdsn.it@gmail.com>
2021-03-14 03:58:33 +0800

    added a button to navigate to the page with users, in case the user does not have a post, as well as clearing the userStore

--- src/pages/posts/posts.tsx
+++ src/pages/posts/posts.tsx
@@ -1,41 +1,48 @@
-import React, { useEffect } from 'react';
-import { ListPosts } from '../../components/list-posts';
-import { useRootStore } from '../../stores/RootStateContext';
-import { Pagination } from '../../components/pagination';
-import { observer } from 'mobx-react-lite';
-import { useParams } from 'react-router';
+import React, {useEffect} from 'react';
+import {ListPosts} from '../../components/list-posts';
+import {useRootStore} from '../../stores/RootStateContext';
+import {Pagination} from '../../components/pagination';
+import {observer} from 'mobx-react-lite';
+import {useParams} from 'react-router';
+import Spinner from '../../components/spinner';
+import { Link } from 'react-router-dom';
 
 interface RouteParams {
-    user_id: string
-    name: string
+    userId: string
 }
 
 export const Posts: React.FC = observer(() => {
-    const { user_id,  name } = useParams<RouteParams>();
-    const { postsStore } = useRootStore();
-    const { posts, isLoading, pagination } = postsStore;
+    const {userId} = useParams<RouteParams>();
+    const {userStore, postsStore} = useRootStore();
+    const {posts, isLoading, pagination} = postsStore;
     const hasPosts = !!posts.length
     const showPagination = !isLoading && hasPosts
     const paginate = (pageNumber: number) => postsStore.loadPosts(pageNumber)
 
     useEffect(() => {
-        postsStore.loadPosts(pagination?.page, Number(user_id));
+        userStore.loadUser(userId)
+        postsStore.loadPosts(pagination?.page, userId);
 
         return () => {
             postsStore.reset()
+            userStore.reset()
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
+    if (userStore.isLoading) {
+        return <Spinner />
+    }
+
     return (
-        <>
-            <ListPosts
-                name={name}
-                posts={posts}
-                isLoading={isLoading}
-            />
-            {
-                showPagination && (
+        hasPosts ? (
+            <>
+                <ListPosts
+                    name={userStore.user?.name}
+                    posts={posts}
+                    isLoading={isLoading}
+                />
+                {showPagination && (
                     <Pagination
                         pages={pagination?.pages}
                         page={pagination?.page}
@@ -43,8 +50,13 @@ export const Posts: React.FC = observer(() => {
                         limit={pagination?.limit}
                         paginate={paginate}
                     />
-                )
-            }
-        </>
-    )
-})
+                )}
+            </>
+        ) : (
+            <>
+                <Link className='btn btn-outline-primary my-2 btn-sm d-flex' to={`/`}>Back</Link>
+                <h4 className='w-100 text-center'>No posts</h4>
+            </>
+        )     
+    );
+});
