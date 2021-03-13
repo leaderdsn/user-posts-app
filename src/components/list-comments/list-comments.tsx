bea6eabf7dc2f3c548f0545672a42ad33917fead
leaderdsn <leaderdsn.it@gmail.com>
2021-03-13 11:30:21 +0800

    commented out the function commentsShow

--- src/components/list-comments/list-comments.tsx
+++ src/components/list-comments/list-comments.tsx
@@ -1,5 +1,4 @@
-import React, { FC, PropsWithChildren } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
 import { Table } from 'react-bootstrap';
 import Spinner from '../spinner';
 import { IComment } from '../../stores/CommentsStore';
@@ -11,72 +10,77 @@ interface ListCommentsProps {
     postId: string
 }
 
-const commentsShow: React.FC<ListCommentsProps> = ({comments, children}) => {
-    comments.map(comment => {
-        const { id, name, body } = comment
-        return (
-            <>
-                <tr key={id}>
-                    <tr>
-                        <td>{id}</td>
-                    </tr>
-                    <tr>
-                        <td>{name}</td>
-                    </tr>
-                    <tr>
-                        <td>{body}</td>
-                    </tr>
-                    {
-                        <tr>
-                            {children ? commentsShow(children): null} 
-                        </tr>
-                    }
-                </tr>
-            </>
-        )
-    })
-}
+// @ts-ignore
+// const commentsShow: React.FC<{comments: IComment[]}> = ({comments}) => {
+//     comments.map(comment => {
+//         const { id, name, body } = comment
+//         return (
+//             <>
+//                 <tr key={id}>
+//                     <tr>
+//                         <td>{id}</td>
+//                     </tr>
+//                     <tr>
+//                         <td>{name}</td>
+//                     </tr>
+//                     <tr>
+//                         <td>{body}</td>
+//                     </tr>
+//                     {
+//                         <tr>
+//                             {
+//                                 // @ts-ignore
+//                                 comment.children ? commentsShow(comment.children): null
+//                             } 
+//                         </tr>
+//                     }
+//                 </tr>
+//             </>
+//         )
+//     })
+//     return
+// }
 
 export const ListComments: React.FC<ListCommentsProps> = ({ postId, comments, isLoading }) => {
 
     return (
         <>
             <h3 className="user-id">Comments</h3>
-            <Table className='list-posts' bordered hover size="sm">
-                <thead>
-                </thead>
-                <tbody>
                     {
                         isLoading ? (
-                            <tr>
-                                <td className='loading-container'>
-                                    <Spinner />
-                                </td>
-                            </tr>
+                            <div className='loading-container'>
+                                <Spinner />
+                            </div>
                         ) : (
-                            commentsShow(comments)
-                            // comments.map(comment => {
-                            //     const { id, name, body } = comment
-                            //     return (
-                            //         <>
-                            //             <tr key={id}>
-                            //                 <tr>
-                            //                     <td>{id}</td>
-                            //                 </tr>
-                            //                 <tr>
-                            //                     <td>{name}</td>
-                            //                 </tr>
-                            //                 <tr>
-                            //                     <td>{body}</td>
-                            //                 </tr>
-                            //             </tr>
-                            //         </>
-                            //     )
-                            // })
+                            // @ts-ignore
+                            // commentsShow(comments)
+                            comments.map(comment => {
+                                const { id, name, body } = comment
+                                return (
+                                    <>
+                                    <Table className='list-post' hover size="sm" responsive>
+                                        <thead>
+
+                                        </thead>
+                                        <tbody>
+                                            <tr key={`comment-${id}`} className='comments-container'>
+                                            </tr>   
+                                            <tr>
+                                                <th className='id-comment align-middle text-center' rowSpan={2}>ID: {id}</th>
+                                                <th className='header-comment'>Name user</th>
+                                                <td>{name}</td>
+                                            </tr>
+                                            <tr>
+                                                <th className='header-comment'>Comment</th>
+                                                <td>{body}</td>
+                                            </tr>
+                                        </tbody>
+                                    </Table>
+                                    </>
+                                )
+                            })
                         )
                     }
-                </tbody>
-            </Table>
         </>
     )
 }
