081df26376f889c0aa97a449eb2e2f8909eea347
leaderdsn <leaderdsn.it@gmail.com>
2021-03-14 04:03:15 +0800

    added User Store and PostStore to Context

--- src/stores/RootStateContext.tsx
+++ src/stores/RootStateContext.tsx
@@ -2,25 +2,31 @@ import React from 'react';
 import { UsersStore } from './UsersStore';
 import { PostsStore } from './PostsStore';
 import { CommentsStore } from './CommentsStore';
+import {UserStore} from './UserStore';
+import {PostStore} from './PostStore';
 
 type RootStateContextValue = {
-    usersStore: UsersStore
-    postsStore: PostsStore
-    commentsStore: CommentsStore
-}
+    userStore: UserStore;
+    usersStore: UsersStore;
+    postStore: PostStore;
+    postsStore: PostsStore;
+    commentsStore: CommentsStore;
+};
 
 const RootStateContext = React.createContext<RootStateContextValue>({} as RootStateContextValue);
 
 export const RootStateProvider: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
+    const userStore = new UserStore();
     const usersStore = new UsersStore();
+    const postStore = new PostStore();
     const postsStore = new PostsStore();
     const commentsStore = new CommentsStore();
 
     return (
-        <RootStateContext.Provider value={{ usersStore, postsStore, commentsStore }}>
+        <RootStateContext.Provider value={{ userStore, usersStore, postStore, postsStore, commentsStore }}>
             {children}
         </RootStateContext.Provider>
     );
 };
 
-export const useRootStore = () => React.useContext(RootStateContext)
+export const useRootStore = () => React.useContext(RootStateContext);
