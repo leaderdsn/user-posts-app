c8882a60a96baf7a7c98abf2c46be2b4251e3f23
leaderdsn <leaderdsn.it@gmail.com>
2021-03-15 03:34:16 +0800

    Changed the Navbar component to a div eleme

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
 import { LoadUsersButton } from '../load-users-button';
-import './menu.css';
 
 export const Menu: React.FC = () => {
     return (
         <>
-            <Navbar className='menu' bg='light' expand='lg'>
+            <div className='menu'>
                 <LoadUsersButton />
-            </Navbar>
+            </div>
         </>
     );
 };
